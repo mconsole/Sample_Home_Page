b958d1538383257b7c0365770d9576f8b115523e
Mitchell Console <Mitchell.Console@gmail.com>
2016-09-21 17:34:58 -0400

    Change navbar logo size for tablets and larget screens

---
+++ script.js
@@ -0,0 +1,10 @@
+//Waits until the DOM is completely loaded before any code is executed
+$(document).ready(function() {
+
+    $(function() {
+        $(".rslides").responsiveSlides({
+            auto: true
+        });
+    });
+
+});
